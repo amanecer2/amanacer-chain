43fe77005affb1bff354afd324fe1b97549d30f4
shahar@sportstraders.io <AJRkuh1986>
2019-01-27 22:45:18 +0200

    init project

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var block_1 = require("./block");
-var block = new block_1.Block(new Date(), 'helllo', 'fsfds', { hello: 'world' });
-console.log(block.toString());
